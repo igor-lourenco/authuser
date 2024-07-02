a8b1eaf1d1bafcad32119dc5a143657d30c088b9
igor-lourenco <igor.lourencosantos@gmail.com>
2024-07-02 11:23:55 -0300

    Implemetação das APIs GET e DELETE

---
+++ src/main/java/com/ead/authuser/controllers/UserController.java
@@ -0,0 +1,49 @@
+package com.ead.authuser.controllers;
+
+import com.ead.authuser.models.UserModel;
+import com.ead.authuser.services.UserServiceInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserServiceInterface userService;
+
+    @DeleteMapping(value = "/{userId}")
+    public ResponseEntity<?> deleteUser(@PathVariable(value = "userId") UUID userId){
+        Optional<UserModel> entity = userService.findById(userId);
+
+        if(entity.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found!");
+        }else{
+            userService.deleteUser(entity.get());
+            return ResponseEntity.ok("User deleted successfully");
+        }
+    }
+
+    @GetMapping(value = "/{userId}")
+    public ResponseEntity<?> getOneuser(@PathVariable(value = "userId") UUID userId){
+        Optional<UserModel> entity = userService.findById(userId);
+
+        if(entity.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found!");
+        }else{
+            return ResponseEntity.ok(entity.get());
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserModel>> findAllUsers(){
+        return ResponseEntity.ok().body(userService.findAll());
+    }
+}
--- src/main/java/com/ead/authuser/services/impl/UserService.java
+++ src/main/java/com/ead/authuser/services/impl/UserService.java
@@ -1,13 +1,33 @@
 package com.ead.authuser.services.impl;
 
+import com.ead.authuser.models.UserModel;
 import com.ead.authuser.repositories.UserRepository;
+import com.ead.authuser.services.UserServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 @Service
-public class UserService {
+public class UserService  implements UserServiceInterface {
 
     @Autowired
     private UserRepository userRepository;
 
+    @Override
+    public Optional<UserModel> findById(UUID userId) {
+        return userRepository.findById(userId);
+    }
+
+    @Override
+    public List<UserModel> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public void deleteUser(UserModel userModel) {
+        userRepository.delete(userModel);
+    }
 }
