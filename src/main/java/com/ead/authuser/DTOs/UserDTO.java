6c494212c0e2f574caa73839ebdf2467db8ea7ef
igor-lourenco <igor.lourencosantos@gmail.com>
2024-07-03 10:40:17 -0300

    Implementação da API - PUT atualizar usuário

--- src/main/java/com/ead/authuser/DTOs/UserDTO.java
+++ src/main/java/com/ead/authuser/DTOs/UserDTO.java
@@ -1,6 +1,7 @@
 package com.ead.authuser.DTOs;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonView;
 import lombok.Data;
 
 import java.util.UUID;
@@ -9,14 +10,37 @@ import java.util.UUID;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserDTO {
 
+    public interface UserView{
+        public static interface RegistrationPost{} // UserDTO Para registrar usuário
+        public static interface UserPut{} // UserDTO para atualizar os dados do usuário
+        public static interface PasswordPut{} // UserDTO para atualizar a senha do usuário
+        public static interface ImagePut{} // UserDTO para atualizar a imagem do usuário
+    }
+
     private UUID userId;
+
+    @JsonView(UserView.RegistrationPost.class)
     private String username;
+
+    @JsonView(UserView.RegistrationPost.class)
     private String email;
+
+    @JsonView({UserView.RegistrationPost.class, UserView.PasswordPut.class})
     private String password;
+
+    @JsonView(UserView.PasswordPut.class)
     private String oldPassword;
+
+    @JsonView({UserView.RegistrationPost.class, UserView.UserPut.class})
     private String fullName;
+
+    @JsonView({UserView.RegistrationPost.class, UserView.UserPut.class})
     private String phoneNumber;
+
+    @JsonView({UserView.RegistrationPost.class, UserView.UserPut.class})
     private String cpf;
+
+    @JsonView(UserView.ImagePut.class)
     private String imageUrl;
 
 }
--- src/main/java/com/ead/authuser/controllers/UserController.java
+++ src/main/java/com/ead/authuser/controllers/UserController.java
@@ -1,12 +1,16 @@
 package com.ead.authuser.controllers;
 
+import com.ead.authuser.DTOs.UserDTO;
 import com.ead.authuser.models.UserModel;
 import com.ead.authuser.services.UserServiceInterface;
+import com.fasterxml.jackson.annotation.JsonView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -19,16 +23,9 @@ public class UserController {
     @Autowired
     private UserServiceInterface userService;
 
-    @DeleteMapping(value = "/{userId}")
-    public ResponseEntity<?> deleteUser(@PathVariable(value = "userId") UUID userId){
-        Optional<UserModel> entity = userService.findById(userId);
-
-        if(entity.isEmpty()){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found!");
-        }else{
-            userService.deleteUser(entity.get());
-            return ResponseEntity.ok("User deleted successfully");
-        }
+    @GetMapping
+    public ResponseEntity<List<UserModel>> findAllUsers(){
+        return ResponseEntity.ok().body(userService.findAll());
     }
 
     @GetMapping(value = "/{userId}")
@@ -42,8 +39,37 @@ public class UserController {
         }
     }
 
-    @GetMapping
-    public ResponseEntity<List<UserModel>> findAllUsers(){
-        return ResponseEntity.ok().body(userService.findAll());
+    @PutMapping(value = "/{userId}")
+    public ResponseEntity<?> updateUser(@PathVariable(value = "userId") UUID userId,
+            @RequestBody @JsonView(UserDTO.UserView.UserPut.class) UserDTO userDTO){
+
+        Optional<UserModel> entityDTO = userService.findById(userId);
+
+        if(entityDTO.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found!");
+        }else{
+
+            var userModel = entityDTO.get();
+            userModel.setFullName(userDTO.getFullName());
+            userModel.setPhoneNumber(userDTO.getPhoneNumber());
+            userModel.setCpf(userDTO.getCpf());
+            userModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
+
+            userService.save(userModel);
+
+            return ResponseEntity.ok(userModel);
+        }
+    }
+
+    @DeleteMapping(value = "/{userId}")
+    public ResponseEntity<?> deleteUser(@PathVariable(value = "userId") UUID userId){
+        Optional<UserModel> entity = userService.findById(userId);
+
+        if(entity.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found!");
+        }else{
+            userService.deleteUser(entity.get());
+            return ResponseEntity.ok("User deleted successfully");
+        }
     }
 }
